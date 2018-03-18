22b28b71e1458a9f3602544d655f24a4e67079ac
unknown <dhirajmittal18@gmail.com>
2018-03-19 00:53:46 +0530

    changes for web-server

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+hbs.registerPartials(__dirname +'/views/partials');
+app.use(express.static(__dirname +'/public'));
+app.set('view engine', 'hbs');
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log +'\n');
+    next();
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        titleName:'About'
+    });
+});
+
+app.get('/bad',(rer,res)=>{
+    res.send({
+        errorMessage:'Unable to handle request'
+    });
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000');
+});
